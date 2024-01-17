8c78454f0094b47374f72f05656669c74a827a86
Manuel Limones <manuel2312034@hybridge.education>
2024-01-16 20:22:43 -0600

    load word for dictionary

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
 import random
 
-word_list = ['papu','pro','vaina','progra','charles']
-MAX_FAILS = 3
+MAX_FAILS = 5
 
 def print_status_bar(strikes, word, correct):
     print_line()
     masked_word = mask_word(word, correct)
-    strikes_string = '[X]'*strikes + '[ ]'* (MAX_FAILS-strikes)
+    strikes_string = create_strikes_board(strikes)
     print('word', masked_word)
     print('strikes', strikes_string)
     print_line()
 
 def mask_word(word, correct):
-    return list(map(lambda c: c if c in correct else '_', list(word)))
+    return list(map(lambda c: c if c in correct else '_', word))
+
+def create_strikes_board(strikes):
+    return '[X]'*strikes + '[ ]'* (MAX_FAILS-strikes)
+
 
 def print_line():
     print("*"*32)
@@ -21,13 +24,14 @@ def select_word(list):
     return random.choice(list)
 
 def do_game():
-    fails = 0
+    strikes = 0
+    word_list = load_list()
     word = select_word(word_list)
     unique_characters = set(word)
     correct_characters = set()
 
-    while(fails < MAX_FAILS):
-        print_status_bar(fails, word, correct_characters)
+    while(strikes < MAX_FAILS):
+        print_status_bar(strikes, word, correct_characters)
         letter = input('enter a character:')
         print('your selected: ',letter)
         if(letter in unique_characters):
@@ -35,20 +39,25 @@ def do_game():
             correct_characters.add(letter)
             print('correct! =)')
             if(len(unique_characters) == 0):
-                print_status_bar(fails, word, correct_characters)
+                print_status_bar(strikes, word, correct_characters)
                 return True
         else:
-            fails+= 1
+            strikes+= 1
             print('strike! =(: ')
     return False
 
 def main():
     if do_game():
         print_line()
-        print('you win ( ͡❛ ͜ʖ ͡❛)')
+        print('you win 😎')
     else:
         print_line()
-        print('you lose ( ͡⊙ ͜ʖ ͡⊙)')
+        print('you lose 😭')
+
+def load_list():
+    file = open('./0_palabras_todas_no_conjugaciones.txt','r')
+    return file.read().splitlines()
+
 
 if __name__ == '__main__':
     main()
